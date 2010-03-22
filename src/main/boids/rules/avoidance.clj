ab298a794e4dd0ddf35938d7b9ad6823e112d821
Scott Shaw <scottwshaw@gmail.com>
2010-03-23 04:18:39 +0530

    Made calculation more readable

--- src/main/boids/rules/avoidance.clj
+++ src/main/boids/rules/avoidance.clj
@@ -4,14 +4,12 @@
 (def avoidance-radius 2.0)
 
 (defn avoidance-adjustment [the-boid bspace] 
-  (let [points-to-avoid
-	(map :location
-	     (remove #(= the-boid %) 
-		     (boids-in-radius bspace 
-				      (:location the-boid) 
-				      avoidance-radius)))
-	zero-point (struct-map spatial-vector :x 0 :y 0)
-	the-point (:location the-boid)]
+  (let [the-point (:location the-boid)
+	points-to-avoid	(map :location
+			     (remove #(= the-boid %) 
+				     (boids-in-radius bspace 
+						      (:location the-boid) 
+						      avoidance-radius)))]
     (apply sv-sum (map #(sv-diff the-point %) points-to-avoid))))
     
 
