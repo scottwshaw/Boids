21056d26b100d0d4b9ffbf571b699b6edab76863
Scott Shaw <scottwshaw@gmail.com>
2010-03-22 09:46:46 +0530

    Made center of mass adjustment factor a global variable that can be shadowed in a thread-local way

--- src/main/boids/rules/center_of_mass.clj
+++ src/main/boids/rules/center_of_mass.clj
@@ -1,7 +1,8 @@
 (ns boids.rules.center-of-mass 
   (:use boids.boid boids.spatial-vector))
 
-(def adjustment-factor 100.0)
+;; I'm assuming this will be defined globally or bound in a thread-local way 
+(def center-of-mass-adjustment-factor 100.0)
 
 (defn center-of-mass [the-boids]
   (sv-div (apply sv-sum (map :location the-boids)) (.length the-boids)))
@@ -11,5 +12,5 @@
     (sv-div (apply sv-sum (map :location the-other-boids)) (.length (vec the-other-boids)))))
 
 (defn center-of-mass-adjustment [this-boid bspace] 
-  (sv-div (center-of-mass-exclusive-of (:boids bspace) this-boid) adjustment-factor))
+  (sv-div (center-of-mass-exclusive-of (:boids bspace) this-boid) center-of-mass-adjustment-factor))
   
--- src/test/boids/rules/center_of_mass_test.clj
+++ src/test/boids/rules/center_of_mass_test.clj
@@ -16,7 +16,15 @@
     (is (<= 3.16665 (:y (center-of-mass-exclusive-of the-boids b1)) 3.16667))
     (is (>= -0.49999 (:x (center-of-mass-exclusive-of the-boids b1)) -0.50001))))
 
-;; x = -0.833333333/100, y = 3.5/100
+;; x = -0.833333333/1000, y = 3.5/1000
+;; This test shadows the center-of-mass-adjustment-factor to make sure it is possible to 
+;; do so.  I'm expecting that these parameters will be injected outside the main function
+;; definition.
+;;
+;; One thing I'm not really understanding right now is why the variable name doesn't have
+;; to be namespace qualified since I'm currently in a different namespace than 
+;; boids.rules.center-of-mass.  I guess the :use thing just unifies all those namespaces?
 (deftest test-should-correctly-calculate-center-of-mass-adjustment
-  (is (= (center-of-mass-adjustment b2 (initial-boid-space))
-	 (struct-map spatial-vector :x -0.008333333333333333 :y 0.035))))
+  (binding [center-of-mass-adjustment-factor 1000.0]
+    (is (= (center-of-mass-adjustment b2 (initial-boid-space))
+	   (struct-map spatial-vector :x -8.333333333333334E-4 :y 0.0035)))))
