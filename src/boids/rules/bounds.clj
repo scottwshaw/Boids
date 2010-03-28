d7b79eecfa79cbab1eb16ffce9b591a736b638a3
Scott Shaw <scottwshaw@gmail.com>
2010-03-28 16:37:54 +0530

    Simplified bounds adjustment approach

--- src/boids/rules/bounds.clj
+++ src/boids/rules/bounds.clj
@@ -4,11 +4,8 @@
 (def bounds-adjustment-factor 1.0)
 
 (defn bounds-adjustment [the-boid the-boid-space]
-  (let [xmin-adjust (if (< (:x (:location the-boid)) (:xmin the-boid-space)) bounds-adjustment-factor 0.0)
-	xmax-adjust (if (> (:x (:location the-boid)) (:xmax the-boid-space)) (- bounds-adjustment-factor) 0.0)
-	ymin-adjust (if (< (:y (:location the-boid)) (:ymin the-boid-space)) bounds-adjustment-factor 0.0)
-	ymax-adjust (if (> (:y (:location the-boid)) (:ymax the-boid-space)) (- bounds-adjustment-factor) 0.0)]
-    (sv-sum (struct spatial-vector xmin-adjust 0.0)
-	    (struct spatial-vector xmax-adjust 0.0)
-	    (struct spatial-vector 0.0 ymin-adjust)
-	    (struct spatial-vector 0.0 ymax-adjust))))
+  (let [x-adjust (if (< (:x (:location the-boid)) (:xmin the-boid-space)) bounds-adjustment-factor 
+		     (if (> (:x (:location the-boid)) (:xmax the-boid-space)) (- bounds-adjustment-factor) 0.0))
+	y-adjust (if (< (:y (:location the-boid)) (:ymin the-boid-space)) bounds-adjustment-factor 
+		     (if (> (:y (:location the-boid)) (:ymax the-boid-space)) (- bounds-adjustment-factor) 0.0))]
+    (struct spatial-vector x-adjust y-adjust)))
