299824e8ab761211797c0bce6e1ebb70f6d78ee5
Scott Shaw <scottwshaw@gmail.com>
2010-04-03 18:03:03 +0530

    Made single frame drawing work

--- src/boids/graphics/draw_boid_space.clj
+++ src/boids/graphics/draw_boid_space.clj
@@ -12,26 +12,27 @@
   (let [d (new Dimension (- (:xmax bspace) (:xmin bspace)) (- (:ymax bspace) (:ymin bspace)))]
     (doto (proxy [JPanel] []) (.setPreferredSize d))))
 
-(defn buffered-render-boid [b g]
-  (let [p0 (sv-diff (:location b) (sv-div (:velocity b) 2.0))
-	p1 (sv-sum (:location b) (sv-div (:velocity b) 2.0))
-        img (new BufferedImage 500 500 (. BufferedImage TYPE_INT_ARGB))
+(defn draw-boid [b g]
+  (let [p0 (sv-diff (:location b) (:velocity b))
+	p1 (sv-sum (:location b) (:velocity b))]
+    (doto g
+      (.setColor (. Color red))
+      (.drawLine (:x p0) (:y p0) (:x p1) (:y p1)))))
+
+(defn render-boid-space [bspace g]
+  (let [xdim (- (:xmax bspace) (:xmin bspace))
+	ydim (- (:ymax bspace) (:ymin bspace))
+	img (new BufferedImage xdim ydim (. BufferedImage TYPE_INT_ARGB))
         bg (. img (getGraphics))]
     (doto bg
       (.setColor (. Color white))
       (.fillRect 0 0 (. img (getWidth)) (. img (getHeight))))
-    (doto bg
-      (.setColor (. Color red))
-      (.drawLine (:x p0) (:y p0) (:x p1) (:y p1)))
+    (dorun (map #(draw-boid % bg) (:boids bspace)))
     (. g (drawImage img 0 0 nil))
     (. bg (dispose))))
 
-(defn render-boid [b g]
-  (let [p0 (sv-diff (:location b) (sv-div (:velocity b) 2.0))
-	p1 (sv-sum (:location b) (sv-div (:velocity b) 2.0))]
-    (doto g
-      (.setColor (. Color white))
-      (.fillRect 0 0 500 500))
-    (doto g
-      (.setColor (. Color red))
-      (.drawLine (:x p0) (:y p0) (:x p1) (:y p1)))))
+(defn render-boid [b bspace g]
+  (doto g
+    (.setColor (. Color white))
+    (.fillRect 0 0 (- (:xmax bspace) (:xmin bspace)) (- (:ymax bspace) (:ymin bspace))))
+  (draw-boid b g))
--- test/boids/graphics/draw_boid_space_test.clj
+++ test/boids/graphics/draw_boid_space_test.clj
@@ -10,9 +10,9 @@
 	    (javax.swing JPanel)))
 
 (defn new-boid [x y vx vy]
-  (struct boid (struct spatial-vector x y) (struct spatial-vector x vy)))
+  (struct boid (struct spatial-vector x y) (struct spatial-vector vx vy)))
 
-(def db1 (new-boid 100 200 40 20))
+(def db1 (new-boid 100 200 4 2))
 (def db2 (new-boid 200 100 3 5))
 (def db3 (new-boid 150 250 5 4))
 (def db4 (new-boid 10 600 1 0))
@@ -23,11 +23,19 @@
   (let [d (new Dimension 
 	       (- (:xmax drawable-boid-space) (:xmin drawable-boid-space)) 
 	       (- (:ymax drawable-boid-space) (:ymin drawable-boid-space)))
-	p (doto (proxy [JPanel] [] (paint [g] (render-boid db1 g))) (.setPreferredSize d))
+	p (doto (proxy [JPanel] [] (paint [g] (render-boid db1 drawable-boid-space g))) (.setPreferredSize d))
 	f (doto (new JFrame) (.add p) .pack .show)]
     (. Thread (sleep 1000))
-    (. f (dispose))
-))
+    (. f (dispose))))
+
+(deftest test-should-render-boid-space
+  (let [d (new Dimension
+	       (- (:xmax drawable-boid-space) (:xmin drawable-boid-space)) 
+	       (- (:ymax drawable-boid-space) (:ymin drawable-boid-space)))
+	p (doto (proxy [JPanel] [] (paint [g] (render-boid-space drawable-boid-space g))) (.setPreferredSize d))
+	f (doto (new JFrame) (.add p) .pack .show)]
+    (. Thread (sleep 1000))
+    (. f (dispose))))
 
 (deftest test-should-create-panel-with-correct-size
   (let [b drawable-boid-space
@@ -35,12 +43,10 @@
 	d (.getPreferredSize p)]
     (is (= d (new Dimension (- (:xmax b) (:xmin b)) (- (:ymax b) (:ymin b)))))))
 
-(deftest test-should-create-a-frame-with-embedded-panel
-  (let [f (doto (new JFrame) (.add (bpanel drawable-boid-space)) .pack .show)]
-    (. Thread (sleep 1000))
-    (. f (dispose))
-    )
-  )
+;(deftest test-should-create-a-frame-with-embedded-panel
+;  (let [f (doto (new JFrame) (.add (bpanel drawable-boid-space)) .pack .show)]
+;    (. Thread (sleep 1000))
+;    (. f (dispose))))
 
 
     
