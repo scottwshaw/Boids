fc2b2bc0b7b008998c0924e6ed869c8fd45e6a60
Scott Shaw <scottwshaw@gmail.com>
2010-04-03 14:40:32 +0530

    Wrote tests for the swing stuff.
    Crappy tests that have thread sleep to visually show the results, but
    no real assertions.

--- src/boids/graphics/draw_boid_space.clj
+++ src/boids/graphics/draw_boid_space.clj
@@ -1,9 +1,37 @@
 (ns boids.graphics.draw-boid-space
-  (:use boids.boid-space)
+  (:use boids.boid-space boids.spatial-vector)
   (:import 
    (java.awt Dimension)
+   (java.awt Color)
+   (java.awt.image BufferedImage)
    (javax.swing JPanel)))
+
+(def red (new Color 255 0 0 255))
 
 (defn bpanel [bspace]
   (let [d (new Dimension (- (:xmax bspace) (:xmin bspace)) (- (:ymax bspace) (:ymin bspace)))]
     (doto (proxy [JPanel] []) (.setPreferredSize d))))
+
+(defn buffered-render-boid [b g]
+  (let [p0 (sv-diff (:location b) (sv-div (:velocity b) 2.0))
+	p1 (sv-sum (:location b) (sv-div (:velocity b) 2.0))
+        img (new BufferedImage 500 500 (. BufferedImage TYPE_INT_ARGB))
+        bg (. img (getGraphics))]
+    (doto bg
+      (.setColor (. Color white))
+      (.fillRect 0 0 (. img (getWidth)) (. img (getHeight))))
+    (doto bg
+      (.setColor (. Color red))
+      (.drawLine (:x p0) (:y p0) (:x p1) (:y p1)))
+    (. g (drawImage img 0 0 nil))
+    (. bg (dispose))))
+
+(defn render-boid [b g]
+  (let [p0 (sv-diff (:location b) (sv-div (:velocity b) 2.0))
+	p1 (sv-sum (:location b) (sv-div (:velocity b) 2.0))]
+    (doto g
+      (.setColor (. Color white))
+      (.fillRect 0 0 500 500))
+    (doto g
+      (.setColor (. Color red))
+      (.drawLine (:x p0) (:y p0) (:x p1) (:y p1)))))
--- test/boids/graphics/draw_boid_space_test.clj
+++ test/boids/graphics/draw_boid_space_test.clj
@@ -6,18 +6,29 @@
 	boids.graphics.draw-boid-space
 	boids.spatial-vector)
   (:import  (java.awt Dimension)
-	    (javax.swing JFrame)))
+	    (javax.swing JFrame)
+	    (javax.swing JPanel)))
 
 (defn new-boid [x y vx vy]
   (struct boid (struct spatial-vector x y) (struct spatial-vector x vy)))
 
-(def db1 (new-boid 100 200 4 2))
+(def db1 (new-boid 100 200 40 20))
 (def db2 (new-boid 200 100 3 5))
 (def db3 (new-boid 150 250 5 4))
 (def db4 (new-boid 10 600 1 0))
 
 (def drawable-boid-space (struct boid-space 0 500 0 500 [db1 db2 db3 db4]))
-     
+
+(deftest test-should-render-single-boid
+  (let [d (new Dimension 
+	       (- (:xmax drawable-boid-space) (:xmin drawable-boid-space)) 
+	       (- (:ymax drawable-boid-space) (:ymin drawable-boid-space)))
+	p (doto (proxy [JPanel] [] (paint [g] (render-boid db1 g))) (.setPreferredSize d))
+	f (doto (new JFrame) (.add p) .pack .show)]
+    (. Thread (sleep 1000))
+    (. f (dispose))
+))
+
 (deftest test-should-create-panel-with-correct-size
   (let [b drawable-boid-space
 	p (bpanel b)
@@ -25,7 +36,12 @@
     (is (= d (new Dimension (- (:xmax b) (:xmin b)) (- (:ymax b) (:ymin b)))))))
 
 (deftest test-should-create-a-frame-with-embedded-panel
-  (let [f (doto (new JFrame) (.add (bpanel drawable-boid-space)) .pack .show)]))
+  (let [f (doto (new JFrame) (.add (bpanel drawable-boid-space)) .pack .show)]
+    (. Thread (sleep 1000))
+    (. f (dispose))
+    )
+  )
+
 
     
 
