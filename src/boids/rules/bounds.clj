d4ddf1bc1759b8c1cdf991068504eb4f56dda483
Scott Shaw <scottwshaw@gmail.com>
2010-04-19 22:10:54 +0530

    Yee-haw.  Tests pass.  Still not through the refactoring though.

--- src/boids/rules/bounds.clj
+++ src/boids/rules/bounds.clj
@@ -1,9 +1,9 @@
 (ns boids.rules.bounds
   (:use boids.spatial-vector))
 
-(defn bounds-adjustment [the-boid the-boid-space]
-  (let [x-adjust (if (< (:x (:location the-boid)) (:xmin the-boid-space)) 1.0
-		     (if (> (:x (:location the-boid)) (:xmax the-boid-space)) -1.0 0.0))
-	y-adjust (if (< (:y (:location the-boid)) (:ymin the-boid-space)) 1.0
-		     (if (> (:y (:location the-boid)) (:ymax the-boid-space)) -1.0 0.0))]
+(defn bounds-adjustment [the-boid the-bounds]
+  (let [x-adjust (if (< (:x (:location the-boid)) (:xmin the-bounds)) 1.0
+		     (if (> (:x (:location the-boid)) (:xmax the-bounds)) -1.0 0.0))
+	y-adjust (if (< (:y (:location the-boid)) (:ymin the-bounds)) 1.0
+		     (if (> (:y (:location the-boid)) (:ymax the-bounds)) -1.0 0.0))]
     (struct spatial-vector x-adjust y-adjust)))
--- test/boids/rules/bounds_test.clj
+++ test/boids/rules/bounds_test.clj
@@ -2,7 +2,7 @@
   (:use clojure.test 
 	boids.boid 
 	boids.boid-test
-	boids.bounds
+	boids.bounds-test
 	boids.rules.bounds
 	boids.spatial-vector))
 
@@ -10,28 +10,28 @@
   (let [loc (struct spatial-vector -11.0 6.0)
 	the-boid (struct-map boid :location loc)
 	expected-adjustment (struct spatial-vector 1.0 0.0)]
-    (is (= (bounds-adjustment the-boid) expected-adjustment))))
+    (is (= (bounds-adjustment the-boid initial-bounds) expected-adjustment))))
 
 (deftest test-should-turn-boid-down-at-top-edge
   (let [loc (struct spatial-vector 0.0 21.0)
 	the-boid (struct-map boid :location loc)
 	expected-adjustment (struct spatial-vector 0.0 -1.0)]
-    (is (= (bounds-adjustment the-boid (initial-boid-space)) expected-adjustment))))
+    (is (= (bounds-adjustment the-boid initial-bounds) expected-adjustment))))
 	
 (deftest test-should-turn-boid-left-at-right-edge
   (let [loc (struct spatial-vector 11.0 6.0)
 	the-boid (struct-map boid :location loc)
 	expected-adjustment (struct spatial-vector -1.0 0.0)]
-    (is (= (bounds-adjustment the-boid (initial-boid-space)) expected-adjustment))))
+    (is (= (bounds-adjustment the-boid initial-bounds) expected-adjustment))))
 
 (deftest test-should-turn-boid-up-at-bottom-edge
   (let [loc (struct spatial-vector 0.0 -1.0)
 	the-boid (struct-map boid :location loc)
 	expected-adjustment (struct spatial-vector 0.0 1.0)]
-    (is (= (bounds-adjustment the-boid (initial-boid-space)) expected-adjustment))))
+    (is (= (bounds-adjustment the-boid initial-bounds) expected-adjustment))))
 
 (deftest test-should-turn-boid-up-and-right-at-lower-left-corner
   (let [loc (struct spatial-vector -11.0 -1.0)
 	the-boid (struct-map boid :location loc)
 	expected-adjustment (struct spatial-vector 1.0 1.0)]
-    (is (= (bounds-adjustment the-boid (initial-boid-space)) expected-adjustment))))
+    (is (= (bounds-adjustment the-boid initial-bounds) expected-adjustment))))
