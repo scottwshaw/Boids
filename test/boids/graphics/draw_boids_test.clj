043b0aabf0b8a839615f1614234714cbecca6bf5
scottwshaw <scottwshaw@gmail.com>
2010-05-17 02:00:32 +0530

    got rid of all the other drawing tests

--- test/boids/graphics/draw_boids_test.clj
+++ test/boids/graphics/draw_boids_test.clj
@@ -10,48 +10,7 @@
 	    (javax.swing JFrame)
 	    (javax.swing JPanel)))
 
-(def db1 (new-boid 100 200 4 2))
-(def db2 (new-boid 200 100 3 5))
-(def db3 (new-boid 150 250 5 4))
-(def db4 (new-boid 10 300 15 10))
-
-(def drawable-bounds (struct bounds 0 1000 0 750))
-(def drawable-boids (atom [db1 db2 db3 db4]))
-
-(deftest test-should-render-single-boid
-  (let [d (new Dimension 
-	       (- (:xmax drawable-bounds) (:xmin drawable-bounds)) 
-	       (- (:ymax drawable-bounds) (:ymin drawable-bounds)))
-	p (doto (proxy [JPanel] [] 
-		  (paint [g] (render-boid db1 drawable-bounds g))) (.setPreferredSize d))
-	f (doto (new JFrame) (.add p) .pack .show)]
-    (. Thread (sleep 500))
-    (. f (dispose))))
-
-(deftest should-render-boids
-  (let [d (new Dimension
-	       (- (:xmax drawable-bounds) (:xmin drawable-bounds)) 
-	       (- (:ymax drawable-bounds) (:ymin drawable-bounds)))
-	p (doto (proxy [JPanel] [] 
-		  (paint [g] (render-boids drawable-bounds drawable-boids g)))
-	    (.setPreferredSize d))
-	f (doto (new JFrame) (.add p) .pack .show)]
-    (. Thread (sleep 500))
-    (. f (dispose))))
-
-(defn render-frame-sequence []
-  (let [d (new Dimension
-	       (- (:xmax drawable-bounds) (:xmin drawable-bounds)) 
-	       (- (:ymax drawable-bounds) (:ymin drawable-bounds)))
-	p (doto (proxy [JPanel] [] 
-		  (paint [g] (render-boids drawable-bounds drawable-boids g)))
-	    (.setPreferredSize d))
-	f (doto (new JFrame) (.add p) .pack .show)]
-    (dotimes [nframes 50]
-      (swap! drawable-boids move-all-boids-one-step drawable-bounds)
-      (. Thread (sleep 100))
-      (. p (repaint)))
-    (. f (dispose))))
+(def drawable-bounds (struct bounds -500 500 -350 350))
 
 (defn render-boids-and-move [boids-a g]
   (binding [*velocity-weight* 0.01
@@ -72,19 +31,6 @@
 	    *avoidance-weight* 2.0]
     (render-boids drawable-bounds boids-a g)
     (swap! boids-a move-all-boids-one-step drawable-bounds)))
-
-(defn render-frame-sequence-with-overrides []
-  (let [d (new Dimension
-	       (- (:xmax drawable-bounds) (:xmin drawable-bounds)) 
-	       (- (:ymax drawable-bounds) (:ymin drawable-bounds)))
-	p (doto (proxy [JPanel] [] 
-		  (paint [g] (render-boids-and-move drawable-boids g)))
-	    (.setPreferredSize d))
-	f (doto (new JFrame) (.add p) .pack .show)]
-    (dotimes [nframes 10]
-      (. Thread (sleep 100))
-      (. p (repaint)))
-    (. f (dispose))))
 
 (defn render-random-frame-sequence-with-overrides []
   (let [d (new Dimension
@@ -102,22 +48,6 @@
 
 (deftest should-render-frame-sequence 
   (render-random-frame-sequence-with-overrides))
-
-(deftest test-should-create-panel-with-correct-size
-  (let [p (bpanel drawable-bounds) d (.getPreferredSize p)]
-    (is (= d (new Dimension 
-		  (- 
-		   (:xmax drawable-bounds) 
-		   (:xmin drawable-bounds)) 
-		  (- 
-		   (:ymax drawable-bounds)
-		   (:ymin drawable-bounds)))))))
-
-;(deftest test-should-create-a-frame-with-embedded-panel
-;  (let [f (doto (new JFrame) (.add (bpanel drawable-boids)) .pack .show)]
-;    (. Thread (sleep 1000))
-;    (. f (dispose))))
-
 
     
 
