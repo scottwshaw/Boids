abd7617e8134fc043bade08ca8d013126e7ca678
scottwshaw <scottwshaw@gmail.com>
2010-05-17 02:06:30 +0530

    This still isn't working for some reason

--- test/boids/graphics/draw_boids_test.clj
+++ test/boids/graphics/draw_boids_test.clj
@@ -12,7 +12,7 @@
 
 (def drawable-bounds (struct bounds -500 500 -350 350))
 
-(defn render-boids-and-move [boids-a g]
+(defn- render-boids-and-move [boids-a g]
   (binding [*velocity-weight* 0.01
 	    *bounds-radius* 100
 	    *bounds-weight* 20
@@ -22,21 +22,11 @@
     (render-boids drawable-bounds boids-a g)
     (swap! boids-a move-all-boids-one-step drawable-bounds)))
 
-(defn render-boids-and-move [boids-a g]
-  (binding [*velocity-weight* 0.01
-	    *bounds-radius* 100
-	    *bounds-weight* 20
-	    *avoidance-radius* 10.0
-	    *center-of-mass-weight* 0.008
-	    *avoidance-weight* 2.0]
-    (render-boids drawable-bounds boids-a g)
-    (swap! boids-a move-all-boids-one-step drawable-bounds)))
-
-(defn render-random-frame-sequence-with-overrides []
+(defn- render-random-frame-sequence-with-overrides []
   (let [d (new Dimension
 	       (- (:xmax drawable-bounds) (:xmin drawable-bounds)) 
 	       (- (:ymax drawable-bounds) (:ymin drawable-bounds)))
-	boids-a (atom (random-boids 10 drawable-bounds 5.0))
+	boids-a (atom (random-boids 40 drawable-bounds 5.0))
 	p (doto (proxy [JPanel] [] 
 		  (paint [g] (render-boids-and-move boids-a g)))
 	    (.setPreferredSize d))
