aa7144804069f368cdbebd597b019b80e235db8f
scottwshaw <scottwshaw@gmail.com>
2010-05-18 08:26:03 +0530

    fixed some tests

--- test/boids/rules/total_test.clj
+++ test/boids/rules/total_test.clj
@@ -10,22 +10,14 @@
 
 (deftest should-compute-correct-adjustment-in-bounds
   (is (= (total-adjustment b2 [b1 b2 b3 b4] initial-bounds)
-	 (struct spatial-vector 7.805  4.525))))
+	 (struct spatial-vector 4.805  -0.47499999999999964))))
 
-(deftest should-compute-correct-adjustment-with-velocity-weight
-  (binding [*velocity-weight* 0.1]
+(deftest should-compute-correct-adjustment-with-weights
+  (binding [*velocity-weight* 0.1
+	    *center-of-mass-weight* 0.2
+	    *avoidance-weight* 0.3]
     (is (= (total-adjustment b2 [b1 b2 b3 b4] initial-bounds)
-	   (struct spatial-vector 4.805 2.725)))))
-
-(deftest should-compute-correct-adjustment-with-com-weight
-  (binding [*center-of-mass-weight* 0.1]
-    (is (= (total-adjustment b2 [b1 b2 b3 b4] initial-bounds)
-	   (struct spatial-vector 7.550000000000001 4.75)))))
-
-(deftest should-compute-correct-adjustment-with-avoidance-weight
-  (binding [*avoidance-weight* 0.1]
-    (is (= (total-adjustment b2 [b1 b2 b3 b4] initial-bounds)
-	   (struct spatial-vector 6.455 6.775)))))
+	   (struct spatial-vector 2.9166666666666665 4.45)))))
 
 (deftest should-compute-correct-adjustment-out-of-bounds
   (let [loc (struct spatial-vector -6.5 6.0)
@@ -34,7 +26,7 @@
 	all-boids [b1 b2 b3 b4]
 	the-bounds (struct bounds -6 10 0 20)]
     (is (= (total-adjustment the-boid all-boids the-bounds) 
-	   (struct spatial-vector 1.8137500000000002 4.71875)))))
+	   (struct spatial-vector 2.8137499999999998 2.71875)))))
 
 (deftest should-compute-correct-adjustment-out-of-bounds-weighted
   (binding [*bounds-weight* 0.1]
@@ -44,4 +36,4 @@
 	  all-boids [b1 b2 b3 b4]
 	  the-bounds (struct bounds -6 10 0 20)]
       (is (= (total-adjustment the-boid all-boids the-bounds) 
-	     (struct spatial-vector 0.9137500000000003 4.71875))))))
+	     (struct spatial-vector 1.9137499999999998 2.71875))))))
