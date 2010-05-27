9ff4eded4570ccd9443fbeb14f5614df72a04b02
Scott Shaw <scottwshaw@gmail.com>
2010-05-27 17:42:56 +0530

    got rid of some multi-arity methods now that everything is using the goal

--- src/boids/boid.clj
+++ src/boids/boid.clj
@@ -7,16 +7,9 @@
 (defn new-boid [x y vx vy]
   (struct boid (struct spatial-vector x y) (struct spatial-vector vx vy)))
 
-(defn move-boid-one-step 
-  ([the-boid all-boids the-bounds]
-     (let [adjustment (total-adjustment the-boid all-boids the-bounds)]
-       (struct boid (sv-sum (:location the-boid) adjustment) adjustment)))
-  ([the-boid all-boids the-bounds the-goal]
+(defn move-boid-one-step [the-boid all-boids the-bounds the-goal]
      (let [adjustment (total-adjustment the-boid all-boids the-bounds the-goal)]
-       (struct boid (sv-sum (:location the-boid) adjustment) adjustment))))
+       (struct boid (sv-sum (:location the-boid) adjustment) adjustment)))
 	  
-(defn move-all-boids-one-step 
-  ([boid-list the-bounds]
-     (for [b boid-list] (move-boid-one-step b boid-list the-bounds)))
-  ([boid-list the-bounds the-goal]
-     (for [b boid-list] (move-boid-one-step b boid-list the-bounds the-goal))))
+(defn move-all-boids-one-step [boid-list the-bounds the-goal]
+     (for [b boid-list] (move-boid-one-step b boid-list the-bounds the-goal)))
