b237d6574aa6f53046eead702b184d5fc1d2132d
scottwshaw <scottwshaw@gmail.com>
2010-06-06 13:32:34 +0530

    started writing mock tests for velocity adjustment

--- src/boids/rules/velocity.clj
+++ src/boids/rules/velocity.clj
@@ -3,8 +3,9 @@
 
 (defn velocity-adjustment [the-boid blist]
   (let [the-other-boids (remove #(= the-boid %) blist)
-	avg-vel (sv-div (apply sv-sum (map :velocity the-other-boids)) 
-			(.length (vec the-other-boids)))]
+	vsum (apply sv-sum (map :velocity the-other-boids))
+	len (.length (vec the-other-boids))
+	avg-vel (sv-div vsum len)]
     (sv-diff avg-vel (:velocity the-boid))))
     
 
--- test/boids/rules/velocity_test.clj
+++ test/boids/rules/velocity_test.clj
@@ -5,8 +5,16 @@
 	boids.spatial-vector)
   (:require [clojure.contrib.mock :as mock]))
 
+
+
 (deftest test-velocity-adjustment
-  (testing "arguments passed to sum"))
+  (testing "arguments passed to sum"
+    (let [sv-sum-args-ex (mock/has-args [(:velocity b1)
+					 (:velocity b3)
+					 (:velocity b4)] (mock/returns {:x 1, :y 1}))]
+      (is (mock/expect
+	   [sv-sum sv-sum-args-ex]
+	   (velocity-adjustment b2 [b1 b2 b3 b4]))))))
 
 ;; send with a tolerance of 0.0 so all boids are updated
 (deftest test-should-match-velocities-of-other-boids
