3acf7757e41555e86736f250de42531987f787c7
scottwshaw <scottwshaw@gmail.com>
2010-06-06 11:35:39 +0530

    started refactoring the velocity adjustment tests

--- src/boids/rules/velocity.clj
+++ src/boids/rules/velocity.clj
@@ -3,7 +3,8 @@
 
 (defn velocity-adjustment [the-boid blist]
   (let [the-other-boids (remove #(= the-boid %) blist)
-	avg-vel (sv-div (apply sv-sum (map :velocity the-other-boids)) (.length (vec the-other-boids)))]
+	avg-vel (sv-div (apply sv-sum (map :velocity the-other-boids)) 
+			(.length (vec the-other-boids)))]
     (sv-diff avg-vel (:velocity the-boid))))
     
 
--- test/boids/rules/velocity_test.clj
+++ test/boids/rules/velocity_test.clj
@@ -1,10 +1,14 @@
 (ns boids.rules.velocity-test
   (:use clojure.test 
-	boids.boid
 	boids.boid-test
 	boids.rules.velocity
-	boids.spatial-vector))
+	boids.spatial-vector)
+  (:require [clojure.contrib.mock :as mock]))
+
+(deftest test-velocity-adjustment
+  (testing "arguments passed to sum"))
 
 ;; send with a tolerance of 0.0 so all boids are updated
 (deftest test-should-match-velocities-of-other-boids
-  (is (= (struct-map spatial-vector :x 0.3333333333333335 :y -3.0) (velocity-adjustment b2 initial-boid-list))))
+  (is (= (velocity-adjustment b2 initial-boid-list)
+	 {:x 0.3333333333333335 :y -3.0} )))
