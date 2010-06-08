ce6c8e081cfc03bd2be3f4ea2b4dc4a62fb91c72
Scott Shaw <scottwshaw@gmail.com>
2010-06-08 12:12:38 +0530

    updated test to ensure self gets included

--- src/boids/rules/avoidance.clj
+++ src/boids/rules/avoidance.clj
@@ -5,14 +5,14 @@
 (defn distance-between-boids [boid-1 boid-2]
   (distance-between (:location boid-1) (:location boid-2)))
 
-(defn boids-in-radius-of-boid [blist the-boid radius]
+(defn boids-in-radius [blist the-boid radius]
   (filter #(<= (distance-between-boids the-boid %) radius) blist))
 
 (defn avoidance-adjustment [the-boid blist avoidance-radius] 
   (let [the-point (:location the-boid)
 	points-to-avoid	(map :location
 			     (remove #(= the-boid %) 
-				     (boids-in-radius-of-boid blist the-boid avoidance-radius)))]
+				     (boids-in-radius blist the-boid avoidance-radius)))]
     (apply sv-sum (map #(sv-diff the-point %) points-to-avoid))))
     
 
--- test/boids/rules/avoidance_test.clj
+++ test/boids/rules/avoidance_test.clj
@@ -14,14 +14,15 @@
 
 (defn d-b-mock-call [_ test-boid]
   (cond (= test-boid b1) (- test-radius 1.0) ; in
+	(= test-boid b2) 0.0 ; in
 	(= test-boid b3) (+ test-radius 1.0) ; out
 	(= test-boid b4) (- test-radius 1.0))) ; in
 
-(deftest test-boids-in-radius 
-  (testing "returns boids that are in"
+(deftest test-boids-in-radius
+  (testing "returns only boids that are in"
     (mock/expect [distance-between-boids (mock/times 3 (mock/calls d-b-mock-call))]
-		 (is (= (boids-in-radius-of-boid [b1 b3 b4] b2 test-radius)
-			(list b1 b4))))))
+		 (is (= (boids-in-radius [b1 b2 b3 b4] b2 test-radius)
+			(list b1 b2 b4))))))
 
 (deftest test-should-move-away-from-other-boid
   (is (= (struct-map spatial-vector :x 1.5 :y -2.5) (avoidance-adjustment b2 initial-boid-list 2.0))))
