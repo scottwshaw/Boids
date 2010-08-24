3b3af80b0023b2902876e54dc111d95c4b1870d0
Scott Shaw <sshaw@shmac.local>
2010-08-24 20:45:38 +1000

    Added midje dependency

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]
-  :dev-dependencies [[leiningen/lein-swank "1.1.0"]])
+  :dev-dependencies [[leiningen/lein-swank "1.1.0"]
+		     [midje/midje "1.0.0-SNAPSHOT"]])
