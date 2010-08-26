b53f6aa2556ca124372fcd10e61cc37de8c0af92
Scott Shaw <sshaw@shmac.corporate.thoughtworks.com>
2010-08-26 13:15:13 +1000

    Wrote my first midje test

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject boids "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]]
-  :dev-dependencies [[leiningen/lein-swank "1.1.0"]
-		     [midje/midje "1.0.0-SNAPSHOT"]])
+                 [org.clojure/clojure-contrib "1.1.0"]
+		 [midje/midje "1.0.0-SNAPSHOT"]]
+  :dev-dependencies [[leiningen/lein-swank "1.1.0"]])
--- test/boids/bounds_test.clj
+++ test/boids/bounds_test.clj
@@ -1,13 +1,14 @@
 (ns boids.bounds-test
-  (:use clojure.test
-	boids.bounds
-	boids.spatial-vector))
+  (:use boids.bounds
+	boids.spatial-vector
+	clojure.test
+	midje.semi-sweet))
 
 (def initial-bounds (struct bounds -10 10 0 20))
 
 (deftest should-correctly-identify-point-inside
   (let [p (struct spatial-vector 1 2)]
-    (is (true? (bounds-contain-point? initial-bounds p)))))
+    (expect (bounds-contain-point? initial-bounds p) => true?)))
 
 (deftest should-correctly-identify-point-outside
   (let [p (struct-map spatial-vector :x -20 :y 2)]
