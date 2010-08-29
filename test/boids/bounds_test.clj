3accdeb85038f236b0a0a8c5de60a832d5b88c1c
Scott Shaw <sshaw@shmac.local>
2010-08-29 23:14:00 +1000

    converted bounds to midje tests

--- test/boids/bounds_test.clj
+++ test/boids/bounds_test.clj
@@ -2,25 +2,26 @@
   (:use boids.bounds
 	boids.spatial-vector
 	clojure.test
+	midje.checkers
 	midje.semi-sweet))
 
 (def initial-bounds (struct bounds -10 10 0 20))
 
 (deftest should-correctly-identify-point-inside
   (let [p (struct spatial-vector 1 2)]
-    (expect (bounds-contain-point? initial-bounds p) => true?)))
+    (expect (bounds-contain-point? initial-bounds p) => true)))
 
 (deftest should-correctly-identify-point-outside
   (let [p (struct-map spatial-vector :x -20 :y 2)]
-    (is (false? (bounds-contain-point? initial-bounds p)))))
+    (expect (bounds-contain-point? initial-bounds p) => false)))
 
 (deftest should-correctly-identify-point-on-y-border
   (let [p (struct-map spatial-vector :x -9 :y 20)]
-    (is (true? (bounds-contain-point? initial-bounds p)))))
+    (expect (bounds-contain-point? initial-bounds p) => true)))
 
 (deftest should-correctly-identify-point-on-corner
   (let [p (struct-map spatial-vector :x -10 :y 20)]
-    (is (true? (bounds-contain-point? initial-bounds p)))))
+    (expect (bounds-contain-point? initial-bounds p) => true)))
 
 
 
