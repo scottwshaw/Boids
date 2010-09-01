b6078f2c6ad066be8d28802177dffafaa7971625
Scott Shaw <scottwshaw@gmail.com>
2010-09-01 19:30:23 +1000

    converted some tests to use midje

--- test/boids/random_test.clj
+++ test/boids/random_test.clj
@@ -1,7 +1,8 @@
 (ns boids.random-test
   (:use clojure.test
 	boids.bounds
-	boids.random))
+	boids.random
+	midje.semi-sweet))
 
 (deftest should-create-a-boid-within-bounds
   (let [xmin 0
@@ -10,10 +11,10 @@
 	ymax 100
 	max-v 10.0
 	rand-boid (random-boid (struct bounds xmin xmax ymin ymax) max-v)]
-    (is (< xmin (:x (:location rand-boid)) xmax))
-    (is (< ymin (:y (:location rand-boid)) ymax))
-    (is (< (- max-v) (:x (:velocity rand-boid)) max-v))
-    (is (< (- max-v) (:y (:velocity rand-boid)) max-v))))
+    (expect (:x (:location rand-boid)) => #(< xmin % xmax))
+    (expect (:y (:location rand-boid)) => #(< ymin % ymax))
+    (expect (:x (:velocity rand-boid)) => #(< (- max-v) % max-v))
+    (expect (:y (:velocity rand-boid)) => #(< (- max-v) % max-v))))
   
 
 (deftest should-create-boids-within-bounds
