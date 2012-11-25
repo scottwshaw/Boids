fe48d3e8292b532e5ce42a26d5d3d84d9b95a833
Scott Shaw <scottwshaw@gmail.com>
2012-11-25 13:47:21 +1100

    updated dependencies and upgraded to clojure 1.2.1

--- project.clj
+++ project.clj
@@ -1,5 +1,3 @@
 (defproject boids "1.0.0-SNAPSHOT"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]]
-  :dev-dependencies [[leiningen/lein-swank "1.1.0"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]])
